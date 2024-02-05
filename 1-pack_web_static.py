4e6417d8298979db5dd86c9dc1d1630f6c620867
CharlesOkechukwu <realcharlieok@gmail.com>
2024-02-04 18:16:56 -0800

    create archive

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""fabfile to create an archive of web_static contents"""
+
+from os import path
+from datetime import datetime
+from fabric.api import local
+
+
+def do_pack():
+    """a fab task to generate an archive of all
+    files in the web_static folder
+    """
+    date = datetime.now()
+    time = date.strftime('%Y%m%d%H%M%S')
+    arch = "versions/web_static_{}.tgz".format(time)
+    if not path.isdir("versions"):
+        if local("mkdir versions").failed:
+            return None
+    cmd = "tar -cvzf {} web_static".format(arch)
+    if local(cmd).failed is True:
+        return None
+    return arch
