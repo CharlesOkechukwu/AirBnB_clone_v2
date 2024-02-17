77e6fbde067e262d75321c49737664ff0439ba5e
CharlesOkechukwu <realcharlieok@gmail.com>
2024-02-17 10:10:40 -0800

    handle two python routes

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Module to start Flask application
+and display HBnB static and Hello HBNB static"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Function to display a string
+    Hello, HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Function to display only hbnb string
+    HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is(text):
+    """print string in url and repl;ace underscores
+    with spaces"""
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def py_url(text='is cool'):
+    """print string when python is added to url
+    with a default value of 'is cool'
+    """
+    return "Python {}".format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
